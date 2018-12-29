5018eecc876cf5df2447fa762813e20872631880
Guli <ignaciomorenopubul@gmail.com>
2018-12-29 21:47:49 +0100

    Ugly testing in production

--- components/Contact.vue
+++ components/Contact.vue
@@ -10,7 +10,26 @@
           </div>
         </div>
         <div class="column is-half">
-          <form name="main-contact" id="contact-form" action="" class="contact-form" method="post" netlify>
+          <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
+                  <input type="hidden" name="form-name" value="contact" />
+                  <p class="hidden">     
+                    <label>Don’t fill this out: <input name="bot-field"></label>   
+                  </p>
+                  <label class="form-label" for="name">
+                    Name:
+                  </label>
+                  <input class="form-field" name="name" id="name" />
+                  <label class="form-label" for="email">
+                    Email:
+                  </label>
+                  <input class="form-field" name="email" id="email" />
+                  <label class="form-label" for="message">
+                    Message:
+                  </label>
+                  <textarea class="form-field" name="message" id="message"></textarea>
+                  <input class="form-button" type="submit" value="Send message" />
+                </form>
+          <!-- <form name="main-contact" id="contact-form" action="" class="contact-form" method="post" netlify>
             <div class="field">
               <label class="label">Name</label>
               <div class="control">
@@ -35,7 +54,7 @@
                 <input type="text" style="display:none" />
               </div>
             </div>
-          </form>
+          </form> -->
         </div>
       </div>      
     </div>
