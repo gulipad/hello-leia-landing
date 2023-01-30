741b9dd16cd49943cd42a07af45b7307b4fbe646
Guli Moreno <guli@capchase.com>
2023-01-30 19:41:10 +0100

    Update Team.vue
    Remove team member

--- components/Team.vue
+++ components/Team.vue
@@ -4,15 +4,15 @@
       <div class="columns is-spaced">
         <div data-aos="fade-in" class="column is-one-third">
           <div class="content">
-            <h2 class="title-serif is-size-1">4. Team</h2>
-            <p class="is-size-4">/tiːm/</p>
+            <h2 class="title-serif is-size-1">4. About me</h2>
+            <p class="is-size-4">/UHbOUt mEE/</p>
             <p class="is-size-5 is-italic">noun</p>
-            <p class="is-size-6 has-text-weight-light">1. a number of persons associated together in work or activity.</p>
+            <p class="is-size-6 has-text-weight-light">1. who I am and what I do.</p>
           </div>
         </div>
         <div data-aos="fade-in" class="column is-half">
           <div class="content">
-            <p class="is-size-5 has-text-weight-light has-text-grey"> A small team with ambitious goals. With a varied background, including <span class="shadow is-success">engineering</span>, <span class="shadow is-warning">design</span> and <span class="shadow is-purple">mathematics</span>, we build digtal solutions with a purpose.</p>
+            <p class="is-size-5 has-text-weight-light has-text-grey"> A varied background, including <span class="shadow is-success">engineering</span>, <span class="shadow is-warning">design</span> and <span class="shadow is-purple">mathematics</span>, we build digtal solutions with a purpose.</p>
             <p class="is-size-5 has-text-weight-light has-text-grey">To take digital humanism to the next level.</p>
           </div>
         </div>
@@ -41,33 +41,6 @@
               </div>
               <div class="content">
                 Aerospace Engineer turned Developer turned Product Manager. I take learnings from my job at a high-growth startup to design, build, and deploy beautiful products. My spare time is spent surfing, playing football, or making music. Technovation volunteer.
-              </div>
-            </div>
-          </div>
-        </div>
-        <div data-aos="fade-up" class="column is-one-half">
-          <div class="card">  
-            <div class="card-content">
-              <div class="media">
-                <div class="media-left">
-                  <figure class="image is-128x128">
-                    <img class="is-rounded" src="../static/myriam.png" alt="Placeholder image">
-                  </figure>
-                </div>
-                <div class="media-content">
-                  <p class="title is-4">Myriam Barnés Guevara </p> 
-                  <p class="subtitle is-6">VC @Kfund</p>
-                  <div class="tags">
-                    <div class="tag is-medium is-light">Venture Capital</div>
-                    <div class="tag is-medium is-light">Mathematics</div>
-                    <a class="is-brand is-size-3 align-hack is-linkedin" href="https://www.linkedin.com/in/myriambarnes/" target="_blank" rel="nofollow">
-                      <font-awesome-icon :icon="['fab', 'linkedin']"/>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div class="content">
-                Industrial Design Engineer, Mathematician and Venture Capitalist. Co-founder of <a href="http://www.reinventatueducation.com" target="_blank" rel="nofollow">reinventatueducacion.com</a>, an initiative to change Spanish education. Humanist at heart, I have a passion for solving social problems. I enjoy writing, the sun and travelling.
               </div>
             </div>
           </div>
